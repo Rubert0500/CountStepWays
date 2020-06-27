c2f4041ada3f4ebbbdce6685c6769e2a8713bc2d
Rubert <ruberttapia0500@gmail.com>
2020-06-27 11:42:56 +0100

    Compromiso inicial

---
+++ Ejercicio1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practica_5
+{
+    public class Ejericio1
+    {
+         public static int Tope(int n) 
+    { 
+        if (n == 1 || n == 0) 
+            return 1; 
+        if (n == 2) 
+            return 2; 
+  
+        else
+        
+            return Tope(n - 3) + Tope(n - 2) + Tope(n - 1); 
+    } 
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Practica_5
+{
+    public class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Introduzca el tope que desea alcanzar: ");
+             int tope = int.Parse(Console.ReadLine()); 
+             Console.WriteLine();
+             int exe1 = Ejericio1.Tope(tope);
+             Console.WriteLine($"La cantidad de combinaciones que el tope {tope} tiene son {exe1}");
+             
+        } 
+    }
+}
